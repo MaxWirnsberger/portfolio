8a8a2ae3743f0b5b7f378c5846bd638f07b5dafe
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-01-22 11:11:56 +0100

    created all projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -4,6 +4,8 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ElpollolocoComponent } from './projects/elpolloloco/elpolloloco.component';
+import { PokedexComponent } from './projects/pokedex/pokedex.component';
+import { CrmComponent } from './projects/crm/crm.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -13,7 +15,9 @@ import { ElpollolocoComponent } from './projects/elpolloloco/elpolloloco.compone
     AboutMeComponent,
     MySkillsComponent,
     ProjectsComponent,
-    ElpollolocoComponent
+    ElpollolocoComponent,
+    PokedexComponent,
+    CrmComponent
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
