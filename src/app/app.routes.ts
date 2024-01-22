74815617e810b8dd62c826a7f61b4d93553920cf
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-01-22 15:27:13 +0100

    added legal notice and privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
 
 export const routes: Routes = [
-    {path: '', component: PortfolioComponent}
+    {path: '', component: PortfolioComponent},
+    {path: 'legalNotice', component: LegalNoticeComponent},
+    {path: 'privacyPolicy', component: PrivacyPolicyComponent},
 ];
