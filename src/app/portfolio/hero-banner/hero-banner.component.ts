edfa50cd2811e9a082d530c19500c3a46e99cb59
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-01-25 15:14:57 +0100

    bugifex

--- src/app/portfolio/hero-banner/hero-banner.component.ts
+++ src/app/portfolio/hero-banner/hero-banner.component.ts
@@ -8,30 +8,34 @@ import { NgFor } from '@angular/common';
   templateUrl: './hero-banner.component.html',
   styleUrl: './hero-banner.component.scss',
 })
-
-// export class HeroBannerComponent implements OnInit, OnDestroy{
 export class HeroBannerComponent {
   arrowImages: string[] = [
-    'assets/imgs/hero-banner/arrow1.png',
-    'assets/imgs/hero-banner/arrow2.png',
-    'assets/imgs/hero-banner/arrow3.png',
-    'assets/imgs/hero-banner/arrow4.png',
-    'assets/imgs/hero-banner/arrow5.png',
-    'assets/imgs/hero-banner/arrow6.png',
-    'assets/imgs/hero-banner/arrow7.png',
+    './../../../assets/imgs/hero-banner/arrow1.png',
+    './../../../assets/imgs/hero-banner/arrow2.png',
+    './../../../assets/imgs/hero-banner/arrow3.png',
+    './../../../assets/imgs/hero-banner/arrow4.png',
+    './../../../assets/imgs/hero-banner/arrow5.png',
+    './../../../assets/imgs/hero-banner/arrow6.png',
+    './../../../assets/imgs/hero-banner/arrow7.png',
   ];
   currentImageIndex = 0;
   private imageSwitchInterval: any;
 
   ngOnInit(): void {
-      this.imageSwitchInterval = setInterval(() => {
-        this.currentImageIndex = (this.currentImageIndex + 1) % this.arrowImages.length;
-      }, 150);
+    this.arrowImages.forEach((imagePath) => {
+      const img = new Image();
+      img.src = imagePath;
+    });
+
+    this.imageSwitchInterval = setInterval(() => {
+      this.currentImageIndex =
+        (this.currentImageIndex + 1) % this.arrowImages.length;
+    }, 150);
   }
 
   ngOnDestroy(): void {
-      if (this.imageSwitchInterval) {
-        clearInterval(this.imageSwitchInterval);
-      }
+    if (this.imageSwitchInterval) {
+      clearInterval(this.imageSwitchInterval);
     }
+  }
 }
